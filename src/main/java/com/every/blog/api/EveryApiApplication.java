359750e88be4c4a73e1c6ee140d7b362f636d33d
Theodore <theodore.park@kakaomobility.com>
2018-05-26 15:43:04 +0900

    project init

---
+++ src/main/java/com/every/blog/api/EveryApiApplication.java
@@ -0,0 +1,12 @@
+package com.every.blog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryApiApplication.class, args);
+	}
+}
